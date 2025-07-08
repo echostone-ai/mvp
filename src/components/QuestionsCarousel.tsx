20acdbdc7adedc38df46c8ce6a781067388dc7c5
echostone-ai <hello@jonathanbraden.com>
2025-07-08 15:48:35 +0300

    Profile - static

--- src/components/QuestionsCarousel.tsx
+++ src/components/QuestionsCarousel.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import styles from './QuestionsCarousel.module.css'
 
